d3a1e9c99fbd6396cd9ef819a031d76a037eab34
David Morgan <dmorgan81@gmail.com>
2015-08-04 10:54:03 -0400

    Initialize spring-boot project

---
+++ src/main/java/io/github/scry/ScryOracleApplication.java
@@ -0,0 +1,12 @@
+package io.github.scry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScryOracleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScryOracleApplication.class, args);
+    }
+}
